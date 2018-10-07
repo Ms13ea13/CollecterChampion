f76fd481c4a69e2f2a2afc5a925e52375c2e251f
GwynblieddLAB\Gwynbliedd <goku.tan@hotmail.com>
2018-10-07 23:38:21 +0700

    - Add Raycast Class - Edit red mat - Create player prefab - Create Cam prefab - Create playerbase class - Edit cube prefab - Add Item / Ground tags - Edit Destroy obj - Create base loop function [leantween recomended] - Use base of playerbase class - add bool function for throw item

--- Assets/Scripts/Stage 1/Target.cs
+++ Assets/Scripts/Stage 1/Target.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class Target : MonoBehaviour
 { 
     private bool _isCheck = false;
-
+    private bool destroyItem;
+    
     public bool GetIsCheck()
     {
         return _isCheck;
     }
 
-    public void SetIsCheck()
+    public void SetIsCheck(bool check)
     {
-        _isCheck = true;
+        _isCheck = check;
+    }
+
+    public void Throw(float force)
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().AddForce(transform.forward * force,ForceMode.Impulse);
+    }
+
+    public void SetDestroy(bool set)
+    {
+        destroyItem = set;
+        if (set)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public bool GetDestroy()
+    {
+        return destroyItem;
     }
 }
