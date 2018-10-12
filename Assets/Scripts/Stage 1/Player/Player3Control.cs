76ea5be66fb2ad90c7acbd4d42377267a1647902
Kancha <40685606+Ms13ea13@users.noreply.github.com>
2018-10-12 20:18:58 +0700

    - set input in unity - add new prefab - add new scene - add script

---
+++ Assets/Scripts/Stage 1/Player/Player3Control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3Control : PlayerBase
+{
+    void Start ()
+    {
+        _playercontrol = GetComponentInParent<CharacterController>();
+        _rigibody = GetComponentInParent<Rigidbody>();
+    }
+	
+	void Update ()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("HorizontalJoy2"), 0, Input.GetAxis("VerticalJoy2"));
+
+        if (_playercontrol.isGrounded)
+        {
+            _velocity.y = 0f;
+
+            if (move != Vector3.zero)
+            {
+                transform.forward = move;
+            }
+        }
+
+        _velocity.x /= 1 + _drag.x * Time.deltaTime;
+        _velocity.y /= 1 + _drag.y * Time.deltaTime;
+        _velocity.z /= 1 + _drag.z * Time.deltaTime;
+        _velocity.y -= _gravity * Time.deltaTime;
+
+        _playercontrol.Move(_velocity * Time.deltaTime + move * Time.deltaTime * _speed);
+    }
+}
