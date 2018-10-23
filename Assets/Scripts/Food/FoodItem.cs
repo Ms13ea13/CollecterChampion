86529c7f2e77d4e9d989cb11a4569ddeb9bb6c56
GwynblieddLAB\Gwynbliedd <goku.tan@hotmail.com>
2018-10-23 18:10:08 +0700

    - create food item

---
+++ Assets/Scripts/Food/FoodItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodItem : MonoBehaviour
+{
+	[SerializeField]
+	private int foodID;
+	
+	[SerializeField]
+	private string FoodName;
+
+	public int GetFoodItemId()
+	{
+		return foodID;
+	}
+
+	public void SetFoodItemId(int id)
+	{
+		foodID = id;
+	}
+
+	public string GetFoodItemName()
+	{
+		return FoodName;
+	}
+
+	public void SetFoodItemName(string name)
+	{
+		FoodName = name;
+	}
+
+
+}
