78b6f55e1cb5c77ffd09cd85febe751342dc5668
Kancha <40685606+Ms13ea13@users.noreply.github.com>
2018-10-23 19:24:25 +0700

    - add food prefab - create spawn point - create spawn food script

---
+++ Assets/Scripts/Food/FoodSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private int foodID;
+    [SerializeField]
+    private GameObject foodPrefab;
+
+    void Start()
+    {
+        SpawnFood();
+    }
+
+    private void SpawnFood()
+    {
+        if (transform.childCount == 0)
+        {
+            GameObject spawnFood = Instantiate(foodPrefab);
+            spawnFood.GetComponent<FoodItem>().SetFoodItemId(foodID);
+            spawnFood.transform.parent = transform;
+            spawnFood.transform.localPosition = Vector3.zero;
+        }
+    }
+}
