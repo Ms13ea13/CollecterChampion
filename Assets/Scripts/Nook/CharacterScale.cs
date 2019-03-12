fd22529ffcbd2e0e0db7c8dadeace9103dfc0aa5
Kancha <40685606+Ms13ea13@users.noreply.github.com>
2019-03-12 23:13:11 +0700

    - fix bug colider (except Shrimp Box) - manage customer and bin position

---
+++ Assets/Scripts/Nook/CharacterScale.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScale : MonoBehaviour
+{
+    void Start()
+    {
+        transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+    }
+}
