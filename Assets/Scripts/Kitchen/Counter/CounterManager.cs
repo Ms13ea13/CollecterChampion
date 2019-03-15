ba1079a9a3a040f1fe4dc0bf495ea5a762eff2a7
GwynblieddLAB\Gwynbliedd <goku.tan@hotmail.com>
2019-03-15 23:09:38 +0700

    - execution order for counter

--- Assets/Scripts/Kitchen/Counter/CounterManager.cs
+++ Assets/Scripts/Kitchen/Counter/CounterManager.cs
@@ -7,12 +7,11 @@ public class CounterManager : InteractableManager
     public override bool Interact(GameObject target, ref bool holding , PlayerController player = null)
     {
         if (target == null) return false;
+        if (target.GetComponent<FoodItem>() != null) // Can be a TrayItem
+            target.GetComponent<FoodItem>().SetFoodOnCounter(true);
         
         SetTargetPosition(target.transform);
         holding = false;
-        
-        if (target.GetComponent<FoodItem>() != null) // Can be a TrayItem
-            target.GetComponent<FoodItem>().SetFoodOnCounter(true);
         return true;
     }
 }
